f2ac367edd27550be97f731d668d78972c312c81
Rahultadmybrand <rahul.thakur@admybrand.com>
2022-11-22 21:55:10 +0530

    env testing

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const app = express();
 
 app.use(morgan("tiny"));
 
-app.get("/", (req, res) => res.status(200).json("Hello world"));
+app.get("/", (req, res) =>
+  res.status(200).json("Hello world" + process.env.API_KEY)
+);
 
 app.listen(process.env.PORT || 3000, () => console.log("Listening"));
